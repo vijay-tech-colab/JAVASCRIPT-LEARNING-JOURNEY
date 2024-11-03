372dec7f6ac627be87fbfd6ec2ee52022c12bf52
vijay kumar <vijaykumar06232@gmail.com>
2024-11-03 18:28:14 +0530

    learnt variable

---
+++ defer_&_sync_keyword/script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+const heading = document.querySelector('h1');
+btn.addEventListener('click',function () {
+    heading.innerHTML = "Paragraph change !"
+});
